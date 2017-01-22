0e8de0cd41b8a5b53dd84e6fa4ce9c95791f0d70
Ishan Mistry <ishanmistry13@gmail.com>
2017-01-22 01:36:54 -0500

    no message

--- server/MoviFlixApi/src/test/java/Tester.java
+++ server/MoviFlixApi/src/test/java/Tester.java
@@ -11,41 +11,44 @@ public class Tester {
 	public static void main(String[] args) {
 
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("MoviFlix-Jpa");
-		
+
 		EntityManager em = emf.createEntityManager();
-		
+
 		User us;
-		
-		//UUID.randomUUID().toString();
-		
-		/*us.setEmailId("ishanmistry13@gmailcom");
-		us.setFirstName("Ishan");
-		us.setLastName("Mistry");
-		us.setUsrPwd("admin");*/
-		
-		/*em.getTransaction().begin();
-		//Similiar to insert into query
-		em.persist(us);
-		
-		em.getTransaction().commit();
-		*/
-		
-		us = em.find(User.class, "ishanmistry13@gmailcom");
-		
+
+		// UUID.randomUUID().toString();
+
 		/*
-		 * For update use merge
+		 * // INSERT RECORD us.setEmailId("ishanmistry13@gmail.com");
+		 * us.setFirstName("Ishan"); us.setLastName("Mistry");
+		 * us.setUsrPwd("admin");
+		 * 
+		 * 
+		 * em.getTransaction().begin(); em.persist(us);
+		 * em.getTransaction().commit();
+		 */
+
+		us = em.find(User.class, "ishanmistry13@gmail.com");
+		
+		us.setFirstName("Dhruv");
+		us.setLastName("Mistry");//This value will not be set as it was already same in the record
+
+		//UPDATE RECORD USING MERGE
 		em.getTransaction().begin();
 		em.merge(us);
-		em.getTransaction().commit();*/
-		
-		//For delete
-		em.getTransaction().begin();
-		em.remove(us);
-		
 		em.getTransaction().commit();
-		
-		
-		
+
+		// For delete
+		/*
+		 * em.getTransaction().begin(); em.remove(us);
+		 * 
+		 * em.getTransaction().commit();
+		 */
+
+		// FIND ALL
+		// Select * from User
+		// em.createQuery("");
+
 		em.close();
 
 		emf.close();
